8714ff94d3545d3e6454527563dd7d5220397cc2
alexsu35 <subbotin0035@gmail.com>
2022-12-21 13:46:47 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #87206]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -14,20 +14,46 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHasNot10ThenMinus1() {
-        int[] data = new int[] {5, 11, 3};
-        int el = 10;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int el = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenArrayHas99Then0() {
-        int[] data = new int[] {5, 10, 28, 14, 99};
-        int el = 99;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 4;
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int el = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[] {10, 2, 4, 8, 4, 14, 4, 3, 22};
+        int el = 22;
+        int start = 0;
+        int finish = data.length - 1;
+        int result = FindLoop.indexInRange(data, el, start, finish);
+        int expected = 8;
         assertThat(result).isEqualTo(expected);
     }
 }
