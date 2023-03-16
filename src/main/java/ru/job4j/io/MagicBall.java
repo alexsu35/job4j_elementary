5548e85fd5322dcce061d32ea77d98a7fa803794
alexsu35 <subbotin0035@gmail.com>
2023-03-16 20:42:27 +0300

    2.0. Чтение из консоли. Класс Scanner. [#179514] Переделка на switch2

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -12,7 +12,7 @@ public class MagicBall {
         switch (answer) {
             case 0 -> System.out.println("Да");
             case 1 -> System.out.println("Нет");
-            case 2 -> System.out.println("Может быть");
+            default -> System.out.println("Может быть");
         }
     }
 }
